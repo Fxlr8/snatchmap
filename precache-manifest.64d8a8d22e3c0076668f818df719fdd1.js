5ce6f59c01831a99d7f8f2d82131bc012af75c23
George Turkin <fxlr8@mail.ru>
2019-12-15 01:40:33 +0300

    Updates

---
+++ precache-manifest.64d8a8d22e3c0076668f818df719fdd1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69020badbacc1cd5b0fc01b0e10bcb37",
+    "url": "/snatchmap/index.html"
+  },
+  {
+    "revision": "56105ce965aa74b264df",
+    "url": "/snatchmap/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "910db079cbeedc905c10",
+    "url": "/snatchmap/static/css/main.e5361b36.chunk.css"
+  },
+  {
+    "revision": "56105ce965aa74b264df",
+    "url": "/snatchmap/static/js/2.0a21db41.chunk.js"
+  },
+  {
+    "revision": "c526727e1c31b499bacd93767e836d0e",
+    "url": "/snatchmap/static/js/2.0a21db41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "910db079cbeedc905c10",
+    "url": "/snatchmap/static/js/main.b51b76db.chunk.js"
+  },
+  {
+    "revision": "4f70c5dcebf1b3c593a7",
+    "url": "/snatchmap/static/js/runtime-main.3a2abef2.js"
+  },
+  {
+    "revision": "97dede31d264cbd8ccba6fdc2fbc784e",
+    "url": "/snatchmap/static/media/building.97dede31.svg"
+  },
+  {
+    "revision": "15cce0debc2d1c67b395330e7eb2a77f",
+    "url": "/snatchmap/static/media/link.15cce0de.svg"
+  },
+  {
+    "revision": "e1a80e4e2cc7a4c45cb040246eb6fa4f",
+    "url": "/snatchmap/static/media/logo.e1a80e4e.svg"
+  },
+  {
+    "revision": "e375abace1cb23855b69227b6bb00ce5",
+    "url": "/snatchmap/static/media/plane.e375abac.svg"
+  },
+  {
+    "revision": "3ca06a9c82e8d78bd029ab26590e49d3",
+    "url": "/snatchmap/static/media/ship.3ca06a9c.svg"
+  }
+]);
