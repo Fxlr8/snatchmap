e9d8cdf8a335121456873a83a20470dc1da1cbb5
Ivan Hilkov <strange.mole@gmail.com>
2019-12-15 16:32:02 +0300

    add objects to map

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
 import React, { FC } from 'react'
-import ReactMapboxGl from 'react-mapbox-gl'
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl'
 import useAxios from 'axios-hooks'
+
+// import plane from 'svg/plane.svg'
+// import ship from 'svg/ship.svg'
+import pin from 'svg/pin.svg'
 
 const MapComponent = ReactMapboxGl({
     accessToken: 'pk.eyJ1IjoiZnhscjgiLCJhIjoiY2s0NGpieWpkMGJ6eDNvbWdvcml1aG1pZSJ9.pAeNPdFX91L9Z33XLXrnbA',
@@ -11,19 +15,39 @@ interface MapProps {
     className?: string
 }
 
+const layoutLayer = { 'icon-image': 'pin' }
+
+const image = new Image()
+image.src = pin
+const images: any = ['pin', image]
+
 const Map: FC<MapProps> = ({ className }) => {
     const [{ data, loading, error }] = useAxios(
         'https://branched-glue.glitch.me/object/objects?fields=link,name,text,personInfo,locationUrl,location'
     )
 
-    console.log(data, loading, error)
+    console.log(data)
 
     return (
         <MapComponent
             style="mapbox://styles/mapbox/light-v10" //eslint-disable-line
             center={[37.627771, 55.7537485]}
             className={className}
-        />
+        >
+            <>
+                {!loading && !error ? (
+                    <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
+                        {data.items.map((item: any) => {
+                            const [ lat, lng ] =item.location.geometry.coordinates
+                            const coordinates = [lat, lng]
+                            return (
+                                <Feature key={item._id} coordinates={coordinates} />
+                            )
+                        })}
+                    </Layer>
+                ) : null}
+            </>
+        </MapComponent>
     )
 }
 
