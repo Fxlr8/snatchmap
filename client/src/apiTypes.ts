3b08b4da50b8045585eb060a5616f336917dc2f3
George Turkin <fxlr8@mail.ru>
2019-12-15 16:27:27 +0300

    property ts type

---
+++ client/src/apiTypes.ts
@@ -0,0 +1,11 @@
+export interface Property {
+    _id: string
+    link: string
+    name: string
+    surname: string
+    price: string
+    locationUrl: string
+    location: any
+    text: string
+    personId: string
+}
