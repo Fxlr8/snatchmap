856573b1f5f0f191ffce34bc44492b9e7a31a4b3
Ivan Hilkov <strange.mole@gmail.com>
2019-12-15 16:03:21 +0300

    add to map axios request for list of objects

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import React, { FC } from 'react'
 import ReactMapboxGl from 'react-mapbox-gl'
+import useAxios from 'axios-hooks'
 
 const MapComponent = ReactMapboxGl({
     accessToken: 'pk.eyJ1IjoiZnhscjgiLCJhIjoiY2s0NGpieWpkMGJ6eDNvbWdvcml1aG1pZSJ9.pAeNPdFX91L9Z33XLXrnbA',
@@ -11,6 +12,12 @@ interface MapProps {
 }
 
 const Map: FC<MapProps> = ({ className }) => {
+    const [{ data, loading, error }] = useAxios(
+        'https://branched-glue.glitch.me/object/objects?fields=link,name,text,personInfo,locationUrl,location'
+    )
+
+    console.log(data, loading, error)
+
     return (
         <MapComponent
             style="mapbox://styles/mapbox/light-v10" //eslint-disable-line
