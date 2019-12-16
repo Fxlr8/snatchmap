d739a66a23e78d6d6726be589bfdf5e7f6be2a60
George Turkin <fxlr8@mail.ru>
2019-12-16 06:08:19 +0300

    Updates

--- precache-manifest.2e7f6ff5a4dd55dcb0bb87ac840c3b21.js
+++ precache-manifest.2e7f6ff5a4dd55dcb0bb87ac840c3b21.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6c58708d77c8bffcae3f26aab69ebd5",
+    "revision": "d4da823381e633892e6129b4929039ea",
     "url": "/snatchmap/index.html"
   },
   {
-    "revision": "7a5036e69d7f4891961d",
+    "revision": "a234f898ce9fb81c31cf",
     "url": "/snatchmap/static/css/2.7efaf445.chunk.css"
   },
   {
-    "revision": "3aa1f0eb0746c5474dbd",
+    "revision": "46e68e73cbc8aac99d0f",
     "url": "/snatchmap/static/css/main.e5361b36.chunk.css"
   },
   {
-    "revision": "7a5036e69d7f4891961d",
-    "url": "/snatchmap/static/js/2.a6e90f6c.chunk.js"
+    "revision": "a234f898ce9fb81c31cf",
+    "url": "/snatchmap/static/js/2.ec82776c.chunk.js"
   },
   {
     "revision": "87fcb2251c97ac2a747d57976edf9eff",
-    "url": "/snatchmap/static/js/2.a6e90f6c.chunk.js.LICENSE"
+    "url": "/snatchmap/static/js/2.ec82776c.chunk.js.LICENSE"
   },
   {
-    "revision": "3aa1f0eb0746c5474dbd",
-    "url": "/snatchmap/static/js/main.df1becd8.chunk.js"
+    "revision": "46e68e73cbc8aac99d0f",
+    "url": "/snatchmap/static/js/main.ba0486d1.chunk.js"
   },
   {
     "revision": "4f70c5dcebf1b3c593a7",
@@ -30,6 +30,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "97dede31d264cbd8ccba6fdc2fbc784e",
     "url": "/snatchmap/static/media/building.97dede31.svg"
+  },
+  {
+    "revision": "29bf1d8d005e93ae054ccf65d4e5d1bb",
+    "url": "/snatchmap/static/media/cross.29bf1d8d.svg"
+  },
+  {
+    "revision": "308c2f821c658029c42a37e580f56a8f",
+    "url": "/snatchmap/static/media/github.308c2f82.svg"
   },
   {
     "revision": "15cce0debc2d1c67b395330e7eb2a77f",
@@ -44,8 +52,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/snatchmap/static/media/pin.ce25c562.svg"
   },
   {
+    "revision": "6770c7ae4de4d1fea569554325b683c0",
+    "url": "/snatchmap/static/media/plane.6770c7ae.svg"
+  },
+  {
     "revision": "6a9cdf8d5fcc2c81c5c9084656eb6257",
     "url": "/snatchmap/static/media/plane.6a9cdf8d.svg"
+  },
+  {
+    "revision": "194cf9881ebce904972235074a19bc19",
+    "url": "/snatchmap/static/media/ship.194cf988.svg"
   },
   {
     "revision": "e3907d7e8077326e3e57e3922948f504",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snatchmap/precache-manifest.807a2e6d444e40e4385067d26662538c.js"
+  "/snatchmap/precache-manifest.2e7f6ff5a4dd55dcb0bb87ac840c3b21.js"
 );
 
 self.addEventListener('message', (event) => {
